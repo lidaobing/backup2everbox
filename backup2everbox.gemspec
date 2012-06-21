93b3bddad8b5e289ea99add4bf5565d6b071728d
LI Daobing <lidaobing@gmail.com>
2012-06-22 00:07:40 +0800

    add backup2everbox binary

--- backup2everbox.gemspec
+++ backup2everbox.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |s|
   s.add_dependency 'activesupport'
   s.add_dependency 'oauth'
   s.add_dependency 'rest-client'
+  s.add_dependency 'launchy'
   s.add_development_dependency "bundler", ">= 1.0.0"
   s.add_development_dependency 'rake', "0.8.7"
   s.add_development_dependency 'rdoc'
---
+++ lib/backup2everbox/cli.rb
@@ -0,0 +1,47 @@
+require 'oauth'
+require 'launchy'
+
+module Backup2everbox
+  class CLI
+    DEFAULT_OPTIONS = {
+      :pwd => '/home',
+      :config_file => '~/.everbox_client/config',
+      :consumer_key => 'GW4YtvBpPwHfY0rCSf2xeOqn7tT0YH2O4zftXCOM',
+      :consumer_secret => 'xlKLpZLVSe0Gk6q4w05PsDpzjEbV8SyE71exgz1i',
+      :oauth_site => 'http://account.everbox.com',
+      :fs_site => 'http://fs.everbox.com',
+      :chunk_size => 1024*1024*4,
+      :proxy => nil
+    }
+
+    def self.execute(stdout, stdin, stderr, arguments = [])
+      self.new.execute(stdout, stdin, stderr, arguments)
+    end
+
+    def initialize(opts={})
+      opts ||= {}
+      @options = DEFAULT_OPTIONS.merge(opts)
+    end
+
+    def execute(stdout, stdin, stderr, arguments = [])
+      request_token = consumer.get_request_token
+      url = request_token.authorize_url
+      puts "open url in your browser: #{url}"
+      Launchy.open(url)
+      STDOUT.write "please input the verification code: "
+      STDOUT.flush
+      verification_code = STDIN.readline.strip
+      access_token = request_token.get_access_token :oauth_verifier => verification_code
+      puts "token: #{access_token.token}"
+      puts "secret: #{access_token.secret}"
+    end
+
+    private
+    def consumer
+      OAuth::Consumer.new @options[:consumer_key], @options[:consumer_secret], {
+        :site => @options[:oauth_site],
+        :proxy => @options[:proxy]
+      }
+    end
+  end
+end
